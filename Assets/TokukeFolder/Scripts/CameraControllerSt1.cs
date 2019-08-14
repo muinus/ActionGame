2f0977a7560a75a8c7e57995e2b167cbe04b1e09
reo0000 <16ec080@ms.dendai.ac.jp>
2019-08-14 16:49:31 +0900

    ステージ１途中

---
+++ Assets/TokukeFolder/Scripts/CameraControllerSt1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerSt1 : MonoBehaviour
+{
+    public GameObject player;
+
+    public GameObject BattleEvent;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!BattleEvent.GetComponent<BattleEvent>().GetIsBattleEvent())
+        {
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 0.75f, -1);
+        }
+    }
+}
