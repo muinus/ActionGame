37cfdd4634226d9c2e8f8e4b1ba2d7f1163fb6b7
muinus <gonkayou1213@gmail.com>
2019-08-18 21:14:00 +0900

    牢屋画像の追加

--- Assets/Script/BattleEventMaster.cs
+++ Assets/Script/BattleEventMaster.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -23,8 +22,6 @@ public class BattleEventMaster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(GetIsBattleEvent());
-        Debug.Log("E;" + eventEndFlag);
 
         if (isBattleEvent)
             if (eventEndFlag)
@@ -83,89 +80,3 @@ public class BattleEventMaster : MonoBehaviour
         this.eventEndFlag = eventEndFlag;
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BattleEventMaster : MonoBehaviour
-{
-    private int enemyCounter;//敵の数
-    private bool isBattleEvent;//イベント中かどうかのフラグ
-    private bool eventEndFlag;//イベント終了フラグ
-
-
-    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        enemyCounter = 0;
-        eventEndFlag = false;
-        isBattleEvent = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //Debug.Log(GetIsBattleEvent());
-        //Debug.Log("E;" + eventEndFlag);
-
-        if (isBattleEvent)
-            if (eventEndFlag)
-            {
-                
-                ResetCamera();//カメラを追従に戻す
-                
-            }
-    }
-
-    void ResetCamera()
-    {
-        isBattleEvent = false;
-    }
-
-    void ResetPlace()
-    {
-        foreach (Transform childTransform in this.transform)
-        {
-            childTransform.gameObject.SetActive(false);
-        }
-    }
-
-    public int GetEnemyCounter()
-    {
-        return enemyCounter;
-    }
-
-    public void IncreaseEnemyCounter()
-    {
-        enemyCounter += 1;
-    }
-
-    public void DecreaseEnemyCounter()
-    {
-        enemyCounter -= 1;
-    }
-
-    public bool GetIsBattleEvent()
-    {
-        return isBattleEvent;
-    }
-
-    public void SetIsBattleEvent(bool isBattleEvent)
-    {
-        this.isBattleEvent = isBattleEvent;
-    }
-
-    public bool GetEventEndFlag()
-    {
-        return eventEndFlag;
-    }
-
-    public void SetEventEndFlag(bool eventEndFlag)
-    {
-        this.eventEndFlag = eventEndFlag;
-    }
-}
->>>>>>> c7bd1f18b472bc7d1f8d7eb73d23a827d9200c3a
