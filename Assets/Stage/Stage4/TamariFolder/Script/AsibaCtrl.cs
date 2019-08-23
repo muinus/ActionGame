ac410ea57e75cfab6a0a064c125c19a3e0ff4784
gomiakuta <true.tiriakuta@gmail.com>
2019-08-23 10:47:49 +0900

    動く足場追加しようとして失敗した失敗した失敗した失敗した

---
+++ Assets/Stage/Stage4/TamariFolder/Script/AsibaCtrl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsibaCtrl : MonoBehaviour
+{
+
+    SurfaceEffector2D surfaceEffector;
+    //Rigidbody2D rb;
+
+    Vector2 movingVector;//足場の移動ベクトルを代入するところ
+    // Start is called before the first frame update
+    void Start()
+    {
+        surfaceEffector = GetComponent<SurfaceEffector2D>();
+        //rb= GetComponent<Rigidbody2D>();
+        setMovingVector(new Vector2(0, 0));
+
+        setMovingVector(new Vector2(0.01f, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        surfaceEffector.speed = movingVector.magnitude;
+        transform.Translate(movingVector);
+    }
+
+    public void setMovingVector(Vector2 v)
+    {
+        movingVector = v;
+    }
+    public Vector2 getMovingVector()
+    {
+
+        return movingVector;
+    }
+
+}
