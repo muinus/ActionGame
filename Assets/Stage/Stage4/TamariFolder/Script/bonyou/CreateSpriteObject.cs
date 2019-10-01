8e45b111249cc828bc26d5a6f35696045a9de406
gomiakuta <true.tiriakuta@gmail.com>
2019-10-01 18:19:15 +0900

    "Ground"レイヤー追加　あとファンネル弾追加（ステージ4　Yキーでデバッグ用で見れます　しかし未完成）

---
+++ Assets/Stage/Stage4/TamariFolder/Script/bonyou/CreateSpriteObject.cs
@@ -0,0 +1,53 @@
+﻿//ファンネルを生成する例
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateSpriteObject : MonoBehaviour
+{
+
+
+    public GameObject instanceTargetObject;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Y))
+        {
+            InstanceFannel();
+        }
+    }
+
+
+    void InstanceFannel()
+    {
+        Vector3 pos = this.transform.position;
+        Quaternion qua = Quaternion.Euler(0,0,90);
+
+        for (int i = 0; i < 7; i++)
+        {
+            GameObject g = GameObject.Instantiate(instanceTargetObject, pos, qua);//ファンネル生成
+
+            float targetHight = (i+1)*0.6f;
+            float riseTime = 0.6f;
+            float bulletSpeed = 5f;
+
+            g.GetComponent<FannelMoveContrl>().setInit(targetHight, riseTime, bulletSpeed);//ファンネルの初期値を与える
+
+            if (targetHight < 0f)
+            {
+
+                Debug.Log("・。・" + targetHight);
+            }
+        }
+    }
+}
