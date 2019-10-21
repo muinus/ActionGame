1b65cfc652f6d4a350c22a309a508bdb22dd6d91
reo0000 <16ec080@ms.dendai.ac.jp>
2019-10-21 10:22:17 +0900

    ステージ１，２，３ボスほぼ完成

---
+++ Assets/Stage/Stage1/Scripts/CameraControllerStage1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerStage1 : MonoBehaviour
+{
+    public GameObject player;
+
+    public GameObject BattleEvent;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!BattleEvent.GetComponent<BattleEventMasterStage1>().GetIsBattleEvent())
+        {
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 0.75f, -1);
+        }
+    }
+}
---
+++ Assets/TokukeFolder/Enemy/hukurou/KazeMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KazeMove : MonoBehaviour
+{
+    float speed = 3.0f;
+    GameObject player;
+    void Start()
+    {
+        
+        Destroy(this.gameObject, 10.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector3.right * Time.deltaTime * speed);
+    }
+}
