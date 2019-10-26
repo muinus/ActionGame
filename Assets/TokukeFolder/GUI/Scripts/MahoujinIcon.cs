28da2eee839dab9ed816c708f119d153016886b6
reo0000 <16ec080@ms.dendai.ac.jp>
2019-10-26 21:09:08 +0900

    GUIの変更

--- Assets/TokukeFolder/GUI/Scripts/MahoujinIcon.cs
+++ Assets/TokukeFolder/GUI/Scripts/MahoujinIcon.cs
@@ -8,13 +8,27 @@ public class MahoujinIcon : MonoBehaviour
     public GameObject fairly;
     public GameObject ookami;
     public GameObject saboten;
+    public GameObject up;
+    public GameObject down;
+    public GameObject right;
+    public GameObject left;
+
+    UIBottun UB_up;
+    UIBottun UB_down;
+    UIBottun UB_left;
+    UIBottun UB_right;
+
     GameObject player;
     bool[] judge={ true, true, true, true };//taka yousei saboten ookami 
     float CT=10.0f;
     private void Start()
     {
         player= GameObject.Find("Player");
-        
+        UB_up = up.GetComponent<UIBottun>();
+        UB_down = down.GetComponent<UIBottun>();
+        UB_right = right.GetComponent<UIBottun>();
+        UB_left = left.GetComponent<UIBottun>();
+
     }
     private void Update()
     {
@@ -23,26 +37,27 @@ public class MahoujinIcon : MonoBehaviour
     public void OnClick()
     {
         //taka
-        if (Input.GetKey(KeyCode.RightArrow) && judge[0] == true)
-        {
-            judge[0] = false;
+
+        if (UB_up.GetIsPressed() && judge[0] == true && SkillLearned.GetSkillActive("SummonTaka") == true)
+            {
+                judge[0] = false;
             Instantiate(taka, new Vector3(player.transform.position.x - 10, player.transform.position.y + 10, player.transform.position.z), Quaternion.identity);
             StartCoroutine("TukaimaCT", 0);
         }
         //yousei
-        if (Input.GetKey(KeyCode.UpArrow) &&  judge[1] == true)
+        if (UB_down.GetIsPressed() &&  judge[1] == true && SkillLearned.GetSkillActive("SummonYosei") == true)
         {
             judge[1] = false;
             Instantiate(fairly, new Vector3(player.transform.position.x + 2, player.transform.position.y, player.transform.position.z), Quaternion.identity);
             StartCoroutine("TukaimaCT", 1);
         }
-        if (Input.GetKey(KeyCode.LeftArrow) && judge[2] == true)
+        if ((UB_right.GetIsPressed()|| UB_left.GetIsPressed()) && judge[2] == true && SkillLearned.GetSkillActive("SummonSaboten") == true)
         {
             judge[2] = false;
             Instantiate(saboten, new Vector3(player.transform.position.x + 2, player.transform.position.y, player.transform.position.z), Quaternion.identity);
             StartCoroutine("TukaimaCT", 2);
         }
-        if (Input.GetKey(KeyCode.DownArrow) && judge[3] == true)
+        if (judge[3] == true && SkillLearned.GetSkillActive("SummonOokami") == true)
         {
             judge[3] = false;
             Instantiate(ookami, new Vector3(player.transform.position.x - 0.1f, player.transform.position.y - 0.1f, player.transform.position.z), Quaternion.identity);
